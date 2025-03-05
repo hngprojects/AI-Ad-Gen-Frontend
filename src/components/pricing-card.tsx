9af1e61a5a123be31fa743b6abb305e203e885ed
Kaylahray <chiomachristopher91@gmail.com>
2025-03-05 07:18:53 +0100

    feat(pricing): added detailed styling, check icon, button  and responsive design

--- src/components/pricing-card.tsx
+++ src/components/pricing-card.tsx
@@ -63,14 +63,80 @@ const tiers: Tier[] = [
 
 export default function PricingCard() {
   return (
-    <div className="mx-auto max-w-6xl w-full">
-      <div className="mx-auto max-w-md mt-10 grid grid-cols-1 gap-6 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+    <div className="mx-auto max-w-6xl w-full ">
+      <div className=" mx-auto max-w-md mt-10 grid grid-cols-1 gap-6 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         {tiers.map((tier) => (
-          <Card key={tier.id}>
-            <CardHeader>
-              <CardTitle>{tier.name}</CardTitle>
-              <CardDescription>{tier.description}</CardDescription>
+          <Card
+            key={tier.id}
+            className={`relative rounded-2xl border gap-0 border-[#E3E3E3] shadow-[0px_1px_3px_2px_rgba(0,0,0,0.06)] px-[1.65rem] py-10  ${
+              tier.mostPopular ? "bg-[#CF54CF]" : "bg-white "
+            } ${
+              tier.id === "tier-basic"
+                ? "bg-[url('/basic-price-bg.svg')] bg-cover bg-center text-white before:absolute before:inset-0 before:bg-black/0  before:z-0"
+                : ""
+            }`}
+          >
+            <CardHeader className="gap-0 py-0 pl-0 pr-4 md:pr-10 xl:pr-19 ">
+              <CardTitle
+                className={`text-[28px] font-semibold leading-[36px] ${
+                  tier.mostPopular ? "text-white" : "text-[#CF54CF]"
+                }`}
+              >
+                {tier.name}
+              </CardTitle>
+              <CardDescription
+                className={` text-sm min-h-10 font-normal leading-5  ${
+                  tier.mostPopular ? "text-white" : "text-[#A1A1A1]"
+                }  `}
+              >
+                {tier.description}
+              </CardDescription>
             </CardHeader>
+            <CardContent className="mt-4  p-0 ">
+              <p
+                className={`flex items-baseline gap-x-2 ${
+                  tier.mostPopular ? "text-white" : "text-[#CF54CF]"
+                } text-[36px] font-semibold leading-[44px]`}
+              >
+                {tier.price === "Free" ? (
+                  "Free"
+                ) : (
+                  <>
+                    ${tier.price}
+                    <span className="text-sm font-normal -ml-1 leading-5">
+                      /mo
+                    </span>
+                  </>
+                )}
+              </p>
+              <div className="bg-[#E3E3E3] h-[1px] w-full mt-[1rem] mb-11" />
+              <ul
+                role="list"
+                className="mt-6 gap-4 flex flex-col text-sm text-gray-600"
+              >
+                {tier.features.map((feature) => (
+                  <li
+                    key={feature}
+                    className={`flex items-center gap-x-[0.9rem] text-nowrap ${
+                      tier.mostPopular ? "text-white" : "text-black"
+                    }  text-base font-normal leading-6 `}
+                  >
+                    <Tick />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+
+            <CardFooter>
+              <Button
+                className="mt-13 mx-auto text-base font-medium leading-[24px]"
+                variant={tier.mostPopular ? "choosePlanOutline" : "choosePlan"}
+                size="choosePlan"
+              >
+                Choose Plan
+              </Button>
+            </CardFooter>
           </Card>
         ))}
       </div>
